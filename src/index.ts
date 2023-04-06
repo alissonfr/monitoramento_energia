450f70ffff950b06c8378f8c6c318ee021012797
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-04-06 01:07:02 -0300

    refactor: refactoring index file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { Monitoramento } from "./controllers/MonitoramentoController";
 
-// Cria uma instância do monitoramento com 3 aparelhos
 let monitoramento = new Monitoramento(2);
 
-// Imprime a lista de aparelhos monitorados
 monitoramento.imprimirAparelhos();
 
-// Imprime a lista de leituras para cada aparelho monitorado
-monitoramento.imprimirLeituras();
+monitoramento.imprimirLeituraAparelhos();
 
-// Imprime o consumo total de cada aparelho monitorado
-monitoramento.imprimirConsumoTotal();
+monitoramento.imprimirLeituraAparelhosCrescente();
 
-// Imprime o consumo total de cada aparelho monitorado
-monitoramento.imprimirConsumoTotalGeral
+monitoramento.imprimirConsumoTotalGeral()
