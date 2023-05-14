55a46d507173738d8aba2834dcd2562a55eca45f
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-05-14 01:38:15 -0300

    feat(cenario.1): verification

--- cenario.1/src/index.ts
+++ cenario.1/src/index.ts
@@ -6,7 +6,36 @@ class Main {
 
     public static main() {
         const monitoramento = new Monitoramento(10);
+        setTimeout(() => {
+            this.verificarAparelhos(monitoramento)
+        }, 2000);
 
+
+    }
+
+    public static verificarAparelhos(monitoramento: Monitoramento) {
+        setInterval(() => {
+            for (let i = 0; i < monitoramento.aparelhos.length; i++) {
+                const aparelho = monitoramento.aparelhos[i];
+                for (let j = 0; j < aparelho.leituras.length; j++) {
+                    const leitura = aparelho.leituras[j];
+                    if (leitura > 0.9 * aparelho.potencia_max) {
+                        console.log(
+                            `🚨 GRAVE: Na leitura ${j + 1} o aparelho ${aparelho.nome} demonstrou estar operando com a potencia de ${leitura}W que é mais de 90% da sua potência máxima (${aparelho.potencia_max}W)`
+                        );
+                    }
+                }
+                console.log(`Leituras do aparelho ${aparelho.nome}: [ ${aparelho.leituras} ]\n`);
+            }
+
+        }, 2000);
+
+        process.on('exit', () => {
+            for (let i = 0; i < monitoramento.aparelhos.length; i++) {
+                const aparelho = monitoramento.aparelhos[i];
+                aparelho.gerarLeiturasWorker.terminate();
+            }
+        });
     }
 }
 
--- cenario.1/src/utils/Worker.ts
+++ cenario.1/src/utils/Worker.ts
@@ -16,13 +16,6 @@ parentPort.on("message", (aparelho: AparelhoData) => {
     const leituras = [];
     for (let i = 1; i <= 10; i++) {
       const consumoVariacao = Math.round(Math.random() * (potencia_max - potencia_min + 1)) + potencia_min;
-
-      // if (consumoVariacao > 0.9 * potencia_max) {
-      //   console.log(
-      //     `🚨 GRAVE: Na leitura ${i} o aparelho ${nome} demonstrou estar operando com a potencia de ${consumoVariacao}W que é mais de 90% da sua potência máxima (${potencia_max}W)`
-      //   );
-      // }
-
       leituras.push(consumoVariacao);
     }
     parentPort.postMessage(leituras);
