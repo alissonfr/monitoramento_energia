917d67c37ce77dd4a88da8d45ddaa5fef27dec8f
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-06-12 23:14:59 -0300

    refactor: decrypt Aparelho objects in main + add cypto lib

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Aparelho } from "./controllers/AparelhoController";
 import { Monitoramento } from "./controllers/MonitoramentoController";
+import { ordenarCrescente } from "./utils/Ordenadores";
+import crypto from "crypto-js"
 
 class Main {
     constructor() { }
@@ -7,13 +9,22 @@ class Main {
     // Complexidade: O(1). Ou seja, o tempo de execução não varia com o tamanho dos dados de entrada.
     public static main() {
         const monitoramento = new Monitoramento(10);
+        const aparelhos: Aparelho[] = []
+
+        for (let i = 0; i < monitoramento.aparelhos.length; i++) {
+            const aparelhoCriptografado = monitoramento.aparelhos[i];
+            const aparelhoDescriptografado = crypto.AES.decrypt(aparelhoCriptografado, monitoramento.getHash()).toString(CryptoJS.enc.Utf8);
+            const aparelhoDecryptado = JSON.parse(aparelhoDescriptografado);
+            aparelhos.push(aparelhoDecryptado);
+        }        
         setTimeout(() => {
-            monitoramento.imprimirAparelhos();
-            monitoramento.imprimirLeituraAparelhos();
-            monitoramento.imprimirLeituraAparelhos();
-            monitoramento.imprimirLeituraAparelhosCrescente();
-            monitoramento.encontrarMaiorDiferenca();
-        }, 3000)
+            this.imprimirAparelhos(aparelhos);
+            this.imprimirLeituraAparelhos(aparelhos);
+            this.imprimirLeituraAparelhosCrescente(aparelhos);
+            this.encontrarMaiorDiferenca(aparelhos);
+        }, 3000);
+    }
+
 
     }
 }
